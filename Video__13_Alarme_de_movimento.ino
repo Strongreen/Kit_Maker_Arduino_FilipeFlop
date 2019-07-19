5f85851cd041c94d12fb190e602de4cd8f9f2da3
Hérika Ströngreen <ka.ah19@hotmail.com>
2019-07-18 21:25:05 -0300

    Vídeo 13 da Série Kit Maker Arduino FilipeFlop

---
+++ Video__13_Alarme_de_movimento.ino
@@ -0,0 +1,22 @@
+// Projeto 13 - Alarme de movimento
+ 
+int pinoTilt = 7;
+int pinoBuzzer = 6;
+ 
+void setup()
+{
+  pinMode(pinoTilt, INPUT);
+}
+ 
+void loop()
+{
+  if(digitalRead(pinoTilt) == HIGH)
+  {
+    tone(pinoBuzzer, 440);
+    delay(1000);       
+  } 
+  else
+  {
+    noTone(pinoBuzzer);   
+  }
+}
