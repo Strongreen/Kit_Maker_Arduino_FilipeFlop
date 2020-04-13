94200b8104287ed9e2d70e015988a622a90379a5
Hérika Ströngreen <strongreen@live.com>
2020-04-12 21:24:30 -0300

    invertendo a lógica

--- Video__10_Sensor_luz_ambiente.ino
+++ Video__10_Sensor_luz_ambiente.ino
@@ -3,22 +3,20 @@
 int pinoLed = 13; 
 int pinoSensorLuz = A0;               
 int valorLuz = 0;                      
- 
+
 void setup()
 {
      pinMode(pinoLed,OUTPUT);        
 }
- 
+
 void loop()
 {
   valorLuz = analogRead(pinoSensorLuz);        
-  if(valorLuz<750)
-  {                
-    digitalWrite(pinoLed,HIGH);
-  }
-  else
-  {                    
+  if(valorLuz < 750 ){                
     digitalWrite(pinoLed,LOW);
+  }
+  else{                    
+    digitalWrite(pinoLed,HIGH);
   }
   delay(10);                   
 }
